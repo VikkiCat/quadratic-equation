b8ae32eb31afcbd20a63c8dec3e35efb7ce49e1e
Viktoria Henyk <viktoria.genuk@gmail.com>
2023-05-04 18:27:40 +0200

    Implemented the solution of quadratic equations

---
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -0,0 +1,33 @@
+package com.epam.rd.autotasks;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        scanner.close();
+
+        double D = Math.pow(b,2)-(4*a*c);
+
+        if (D>0){
+            double x1 = ((-b+(Math.sqrt(D)))/(2*a));
+            double x2 = ((-b-(Math.sqrt(D)))/(2*a));
+
+            System.out.println(x1+" "+x2);
+        } else if (D==0) {
+            double x = (-b)/2*a;
+            System.out.println(x);
+        } else if (D<0) {
+            System.out.println("no roots");
+        }
+
+    }
+
+}
